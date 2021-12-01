dc6789a2d81863a1fc93694ede192d3ef2f191c5
Gray <>
2021-12-01 20:29:47 +0600

    Initial Commit

---
+++ abc/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  abc
+//
+//  Created by ablai erzhanov on 01.12.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Tested some code")
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
